7143b3ac3cbf6fb4944cae0524471a350901daf5
thierryb49 <thierry_bouillon@hotmail.com>
2019-11-07 08:03:14 +0100

    first commit

---
+++ src/main/java/org/tb/pim/ProjectImmoManagerApplication.java
@@ -0,0 +1,13 @@
+package org.tb.pim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectImmoManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectImmoManagerApplication.class, args);
+	}
+
+}
